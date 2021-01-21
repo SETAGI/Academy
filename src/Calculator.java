6a38c6137458c3ac70b697fc4869413c395dbedf
SETAGI <tamayo0711@hotmail.com>
2021-01-21 00:40:39 -0500

    sum and subtract done

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,11 +7,19 @@ public class Calculator {
         this.b = b;
     }
 
+    public void sum(){
+        System.out.println("The result of the sum is: "+ this.a + this.b);
+    }
+
+    public void subtract(){
+        System.out.println("The result of the sum is: "+ (this.a - this.b));
+    }
+
     public void multiplication(){
         System.out.println("The result of the multiplication is: "+ this.a * this.b);
     }
 
     public void divide(){
-        System.out.println("The result of the multiplication is: "+ this.a / this.b);
+        System.out.println("The result of the division is: "+ this.a / this.b);
     }
 }
